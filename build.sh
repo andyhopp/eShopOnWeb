a23a110cc52e785c3a3fd406088f9eb4fbb7494e
Andy Hopper <andyhopp@amazon.com>
2019-03-28 20:53:38 -0400

    Removed machine-specific build script.

--- build.sh
+++
@@ -1,7 +0,0 @@
-rm -rf bin/Release/netcoreapp2.2/linux-arm64/publish/*
-dotnet publish -c Release -r linux-arm64
-pushd bin/Release/netcoreapp2.2/linux-arm64/publish
-zip -r eShopOnWeb.zip *
-popd
-scp -i ~/WorkDocs/andyhopp.pem bin/Release/netcoreapp2.2/linux-arm64/publish/eShopOnWeb.zip ubuntu@ec2-54-210-180-171.compute-1.amazonaws.com:~/eShopWeb/eShopOnWeb.zip
-
