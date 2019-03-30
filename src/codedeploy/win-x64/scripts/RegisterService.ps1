3174d40915e779ffa25ef58800cb7cfd03caaf14
Andy Hopper <andyhopp@amazon.com>
2019-03-29 21:18:06 -0400

    Added processor architecture to footer, added firewall rule to allow port 80 to Windows regsitration.

--- src/codedeploy/win-x64/scripts/RegisterService.ps1
+++ src/codedeploy/win-x64/scripts/RegisterService.ps1
@@ -1,2 +1,4 @@
 $ErrorActionPreference = 'Stop'
 New-Service -Name eShopOnWeb -BinaryPathName "c:\eShopOnWeb\Web.exe --service" -DisplayName "eShopOnWeb Sample App" -StartupType Automatic 
+# allow HTTP through the Windows firewall
+netsh advfirewall firewall add rule name = "Open HTTP" dir=in action=allow protocol=TCP localport=80
