4b4e2b1d524b394387b9f8efc54f9baaac5d9855
Andy Hopper <andyhopp@amazon.com>
2019-03-30 18:35:44 -0400

    Added flag to curl call to retry on ECONNREFUSED.

--- src/codedeploy/linux-arm64/scripts/smokeTest.sh
+++ src/codedeploy/linux-arm64/scripts/smokeTest.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-exit curl http://localhost:80 || 1
+curl -s -S \
+  --connect-timeout 5 \
+  --max-time 10 \
+  --retry-connrefused \
+  --retry 5 \
+  --retry-delay 0 \
+  --retry-max-time 40 \
+  http://localhost:80 \
+  > /dev/null
